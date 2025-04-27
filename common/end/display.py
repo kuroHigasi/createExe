8e587178e4813aeac8cae550a30d8554d7e6a952
Kurosu <kurosann1939@outlook.jp>
2025-04-27 23:35:24 +0900

    [0005] END処理 構造改善

--- common/end/display.py
+++ common/end/display.py
@@ -4,24 +4,27 @@ import common.common as cmn
 
 
 class Display:
-    def endDisplay(screen, endForm, opeForm, posX: int, posY: int):
-        imgList = endForm.get_img_list()
-        font = endForm.get_font()
-        screen.blit(imgList[INDEX.END()][0], (posX, posY))
+    @staticmethod
+    def endDisplay(screen, end_form, ope_form, pos_x: int, pos_y: int):
+        img_list = end_form.get_img_list()
+        font = end_form.get_font()
+        screen.blit(img_list[INDEX.END()][0], (pos_x, pos_y))
         # TEXT
-        Display.__dispText(screen, font, "総行動数:" + str(endForm.get_count()), 50, 200)
+        Display.__disp_text(screen, font, "総行動数:" + str(end_form.get_count()), 50, 200)
         # HOME
-        Display.__dispButton(screen, imgList, opeForm, 4, 50, 670)
-        endForm.set_home_button(50, 670)
+        Display.__disp_button(screen, img_list, ope_form, 4, 50, 670)
+        end_form.set_home_button(50, 670)
 
-    def __dispButton(screen, imgList, opeForm, buttonIndex: int, posX: int, posY: int):
-        (x, y) = opeForm.get_mouse()
-        if judge.hitJudgeSquare(posX, posY, 200, 80, int(x), int(y)):
-            screen.blit(imgList[INDEX.BUTTON()][buttonIndex+1], (posX, posY))
+    @staticmethod
+    def __disp_button(screen, img_list, ope_form, index: int, pos_x: int, pos_y: int):
+        (x, y) = ope_form.get_mouse()
+        if judge.hitJudgeSquare(pos_x, pos_y, 200, 80, int(x), int(y)):
+            screen.blit(img_list[INDEX.BUTTON()][index+1], (pos_x, pos_y))
         else:
-            screen.blit(imgList[INDEX.BUTTON()][buttonIndex], (posX, posY))
+            screen.blit(img_list[INDEX.BUTTON()][index], (pos_x, pos_y))
 
-    def __dispText(screen, font, text: str, x: int, y: int):
+    @staticmethod
+    def __disp_text(screen, font, text: str, x: int, y: int):
         text_surface = font.render(text, True, cmn.Colors.black)
         text_rect = text_surface.get_rect(center=(x+text_surface.get_width()/2, y))
         screen.blit(text_surface, text_rect)
